2107c3a36cdf77f605c530939114bbd03aefc934
yam501 <ya.misha501@gmail.com>
2023-10-30 22:53:59 +0500

    PropRandomizerScript

--- CyberneticWanderers/Assets/_Scripts/PropRandomizer.cs
+++ CyberneticWanderers/Assets/_Scripts/PropRandomizer.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class PropRandomizer : MonoBehaviour
 {
-    
+    public List<GameObject> propSpawnPoints;
+    public List<GameObject> propPrefab;
 
     void Start()
+    {
+        SpawnProps();
+    }
+
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnProps()
     {
-        
+        foreach (GameObject sp in propSpawnPoints)
+        {
+            int rand = Random.Range(0, propPrefab.Count);
+            Instantiate(propPrefab[rand], sp.transform.position, Quaternion.identity);
+        }
     }
 }
