7e68d02b01e3a7db6ec149f6d1fe663ac4a86266
Mirel <valerymirel@mail.ru>
2023-11-02 11:46:40 +0500

    Added base gamplay logic
    Враги теперь получают укрон и умирают
    Добавлены логики и скриптбобжекты для ножа и чеснока

--- CyberneticWanderers/Assets/_Scripts/Weapons/Weapon Base/MeleeWeaponBehavior.cs
+++ CyberneticWanderers/Assets/_Scripts/Weapons/Weapon Base/MeleeWeaponBehavior.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class MeleeWeaponBehavior : MonoBehaviour
 {
-
+    public WeaponScriptableObject weaponData;
     public float destroyAfterSeconds;
-    
+
+    //Current stats
+    protected float currentDamage;
+    protected float currentSpeed;
+    protected float currentCooldownDuration;
+    protected float currentPierce;
+
+
+    private void Awake()
+    {
+        currentDamage = weaponData.Damage;
+        currentSpeed = weaponData.Speed;
+        currentPierce = weaponData.Pierce;
+        currentCooldownDuration = weaponData.CooldownDuration;
+    }
+
     protected virtual void Start()
     {
         Destroy(gameObject, destroyAfterSeconds);
+    }
+
+    protected virtual void OnTriggerEnter2D(Collider2D col)
+    {
+        EnemyStats enemy = col.GetComponent<EnemyStats>();
+        enemy.TakeDamage(currentDamage);
     }
 }
--- CyberneticWanderers/Assets/_Scripts/Weapons/Weapon Behavior/GarlcBehavior.cs
+++ CyberneticWanderers/Assets/_Scripts/Weapons/Weapon Behavior/GarlcBehavior.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class GarlcBehavior : MeleeWeaponBehavior
 {
+    List<GameObject> markedEnemies;
 
     protected override void Start()
     {
         base.Start();
+        markedEnemies = new List<GameObject>();
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Enemy") && !markedEnemies.Contains(col.gameObject))
+        {
+            EnemyStats enemy = col.GetComponent<EnemyStats>();
+            enemy.TakeDamage(currentDamage);
+
+            markedEnemies.Add(col.gameObject); //Пометить врага чтобы не наносить урон сразу несколько раз
+        }
     }
 }
