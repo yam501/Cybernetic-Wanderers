5d418af2ec043f2afcbb59e320fd645d708588c9
Mirel <valerymirel@mail.ru>
2023-11-29 17:56:04 +0500

    Enemy spawner start

---
+++ CyberneticWanderers/Assets/_Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemySpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public class Wave
+    {
+        public string waveName;
+        public List<EnemyGroup> enemyGroups;
+        public int waveQuota;   //Всего врагов за волну
+        public float spawnInterval;
+        public int spawnCount;  //Сколько врагов уже заспавнилось
+    }
+
+    [System.Serializable]
+    public class EnemyGroup
+    {
+        public string enemyName;
+        public int enemyCount;
+        public int spawnCount;
+        public GameObject enemyPrefab;
+    }
+
+    public List<Wave> waves;
+    public int currentWaveCount;
+
+    Transform player;
+
+    void Start()
+    {
+        //player = FindObjectOfType<PlayerStats>().transform;
+        CalculateWaveQuota();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void CalculateWaveQuota()
+    {
+        int currentWaveQuota = 0;
+        foreach (var enemyGroup in waves[currentWaveCount].enemyGroups)
+        {
+            currentWaveQuota += enemyGroup.enemyCount;
+        }
+
+        waves[currentWaveCount].waveQuota = currentWaveQuota;
+    }
+
+    void SpawnEnemies()
+    {
+        if (waves[currentWaveCount].spawnCount < waves[currentWaveCount].waveQuota)
+        {
+            foreach (var enemyGroup in waves[currentWaveCount].enemyGroups)
+            {
+                if (enemyGroup.spawnCount < enemyGroup.enemyCount)
+                {
+
+                }
+            }
+        }
+    }
+}
